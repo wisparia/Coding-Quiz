3d4b3e6421489d413cef2bf1622df59c5fab3d57
samis <samilsully@gmail.com>
2020-09-08 18:35:42 -0700

    Added in the working timer

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log("Hello!")
+var questions = [
+    {
+    question : "Awnser this question A",
+    choices : ["Awnser A", "Awnser B", "Awnser C", "Awnser D"],
+    awnser : "b",
+},
+{
+    question : "Awnser this question B",
+    choiceA : "Awnser A",
+    choiceB : "Awnser B",
+    choiceC : "Awnser C",
+    choiceD : "Awnser D",
+},
+{
+    question : "Awnser this question C",
+    choiceA : "Awnser A",
+    choiceB : "Awnser B",
+    choiceC : "Awnser C",
+    choiceD : "Awnser D",
+    correct : "C"
+},
+{
+    question : "Awnser this question D",
+    choiceA : "Awnser A",
+    choiceB : "Awnser B",
+    choiceC : "Awnser C",
+    choiceD : "Awnser D",
+    correct : "D",
+}]; 
+
+
+var startQuiz = document.getElementById("startQuiz");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var start = document.getElementById("start");
+
+var choiceA = document.getElementById("choiceA");
+var choiceB = document.getElementById("choiceB");
+var choiceC = document.getElementById("choiceC");
+var choiceD = document.getElementById("choiceD");
+var timeLeft = document.getElementById("time");
+var highscores = document.getElementById("highscores");
+var score = 0;
+var secondsLeft = 60;
+var timerInterval;
+
+start.addEventListener("click", function(){
+        startQuiz.style = "display: none";
+        quiz.style = "display: block";
+        startTimer();
+    })
+
+    function startTimer() {
+        //starts the timer
+        timerInterval = setInterval(function() {
+            secondsLeft--;
+            timeLeft.innerHTML = "Time: " + secondsLeft;
+            // stops the timer at 0 seconds left
+            if(secondsLeft < 1 ) {
+            clearInterval(timerInterval);
+            }
+        }, 1000);
+    }
