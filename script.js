8e8b8227323ab011b3bfa4d0fb717a450fcfce98
samis <samilsully@gmail.com>
2020-09-08 19:54:52 -0700

    Populating the questions and awnsers into the buttons

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
 var questions = [
-    {
-    question : "Awnser this question A",
-    choices : ["Awnser A", "Awnser B", "Awnser C", "Awnser D"],
-    awnser : "b",
-},
-{
-    question : "Awnser this question B",
-    choiceA : "Awnser A",
-    choiceB : "Awnser B",
-    choiceC : "Awnser C",
-    choiceD : "Awnser D",
-},
-{
-    question : "Awnser this question C",
-    choiceA : "Awnser A",
-    choiceB : "Awnser B",
-    choiceC : "Awnser C",
-    choiceD : "Awnser D",
-    correct : "C"
-},
-{
-    question : "Awnser this question D",
-    choiceA : "Awnser A",
-    choiceB : "Awnser B",
-    choiceC : "Awnser C",
-    choiceD : "Awnser D",
-    correct : "D",
-}]; 
-
+  {
+    question: "Awnser this question A",
+    choices: ["Awnser A1", "Awnser B1", "Awnser C1", "Awnser D1"],
+    correct: "Awnser A1",
+  },
+  {
+    question: "Awnser this question B",
+    choices: ["Awnser A2", "Awnser B2", "Awnser C2", "Awnser D2"],
+    correct: "Awnser A2",
+  },
+  {
+    question: "Awnser this question C",
+    choices: ["Awnser A3", "Awnser B3", "Awnser C3", "Awnser D3"],
+    correct: "Awnser C3",
+  },
+  {
+    question: "Awnser this question D",
+    choices: ["Awnser A4", "Awnser B4", "Awnser C4", "Awnser D4"],
+    correct: "Awnser A4",
+  },
+];
 
 var startQuiz = document.getElementById("startQuiz");
 var quiz = document.getElementById("quiz");
-var question = document.getElementById("question");
+var titleQuestion = document.getElementById("titleQuestion");
 var start = document.getElementById("start");
 
 var choiceA = document.getElementById("choiceA");
@@ -40,24 +32,36 @@ var choiceC = document.getElementById("choiceC");
 var choiceD = document.getElementById("choiceD");
 var timeLeft = document.getElementById("time");
 var highscores = document.getElementById("highscores");
+var questionNumber = 0;
 var score = 0;
 var secondsLeft = 60;
 var timerInterval;
 
-start.addEventListener("click", function(){
-        startQuiz.style = "display: none";
-        quiz.style = "display: block";
-        startTimer();
-    })
+start.addEventListener("click", function () {
+  startQuiz.style = "display: none";
+  startTimer();
+  displayQuiz();
+});
 
-    function startTimer() {
-        //starts the timer
-        timerInterval = setInterval(function() {
-            secondsLeft--;
-            timeLeft.innerHTML = "Time: " + secondsLeft;
-            // stops the timer at 0 seconds left
-            if(secondsLeft < 1 ) {
-            clearInterval(timerInterval);
-            }
-        }, 1000);
+function startTimer() {
+  //starts the timer
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeLeft.innerHTML = "Time: " + secondsLeft;
+    // stops the timer at 0 seconds left
+    if (secondsLeft < 1) {
+      clearInterval(timerInterval);
     }
+  }, 1000);
+}
+
+function displayQuiz () { 
+    titleQuestion.textContent = questions[questionNumber].question;
+    console.log(titleQuestion);
+    choiceA.textContent = questions[questionNumber].choices[0];
+    choiceB.textContent = questions[questionNumber].choices[1];
+    choiceC.textContent = questions[questionNumber].choices[2];
+    choiceD.textContent = questions[questionNumber].choices[3];
+    quiz.style = "display: block";
+}
+
